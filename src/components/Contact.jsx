9eedec647a343166a5490d47fb9a81efeee75e36
ApiradeeH <apiradeehaeusler@gmail.com>
2023-07-14 13:06:20 +0200

    add contact page

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,7 +1,219 @@
-import React from "react";
+import React, { useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faFacebook,
+  faTwitter,
+  faInstagram,
+  faLinkedin,
+  faWhatsapp,
+} from "@fortawesome/free-brands-svg-icons";
+import styled from "styled-components";
+import contactbg from "../assets/contactbg.png"; // Import your background image
 
-function Contact() {
-  return <div>Contact</div>;
-}
+const Contact = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Process the form submission
+    // You can add your logic here, such as sending the data to a server or displaying a success message
+    console.log("Form submitted!");
+    console.log("Name:", name);
+    console.log("Email:", email);
+    console.log("Message:", message);
+    // Reset the form fields
+    setName("");
+    setEmail("");
+    setMessage("");
+  };
+
+  return (
+    <Wrapper>
+      <div className="container py-5 gap-5">
+        <div className="row ms-1">
+          <div className="border col-md-6 d-flex flex-column justify-content-center">
+            <h1>Contact Us</h1>
+            <form onSubmit={handleSubmit}>
+              <div className="mb-3">
+                <label htmlFor="name" className="form-label">
+                  Name:
+                </label>
+                <input
+                  type="text"
+                  className="form-control"
+                  id="name"
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}
+                  required
+                />
+              </div>
+
+              <div className="mb-3">
+                <label htmlFor="email" className="form-label">
+                  Email:
+                </label>
+                <input
+                  type="email"
+                  className="form-control"
+                  id="email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  required
+                />
+              </div>
+
+              <div className="mb-3">
+                <label htmlFor="message" className="form-label">
+                  Message:
+                </label>
+                <textarea
+                  className="form-control"
+                  id="message"
+                  value={message}
+                  onChange={(e) => setMessage(e.target.value)}
+                  required
+                ></textarea>
+              </div>
+
+              <button type="submit" className="btn">
+                Submit
+              </button>
+            </form>
+          </div>
+
+          <div className="col-md-6">
+            <div className="border p-4">
+              <h2>Company Details</h2>
+              <p>
+                <a
+                  href="mailto:bloomexpress2023@gmail.com"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Email: bloomexpress2023@gmail.com
+                </a>
+              </p>
+              <p>Telephone: +1 123-456-7890</p>
+              <p>
+                <FontAwesomeIcon icon={faWhatsapp} />
+                <a
+                  href="https://wa.me/1234567890"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  WhatsApp: +1 123-456-7890
+                </a>
+              </p>
+              <div className="social-icons">
+                <a
+                  href="https://www.facebook.com"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <FontAwesomeIcon icon={faFacebook} size="2x" />
+                </a>
+                <a
+                  href="https://www.twitter.com"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <FontAwesomeIcon icon={faTwitter} size="2x" />
+                </a>
+                <a
+                  href="https://www.instagram.com"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <FontAwesomeIcon icon={faInstagram} size="2x" />
+                </a>
+                <a
+                  href="https://www.linkedin.com"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <FontAwesomeIcon icon={faLinkedin} size="2x" />
+                </a>
+              </div>
+            </div>
+
+            <div className="mt-4">
+              <h2>Address</h2>
+              <p>123 Main Street, City, Country</p>
+              <p>
+                <a
+                  href="https://www.google.com/maps"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Find us on Google Maps
+                </a>
+              </p>
+              <iframe
+                title="Google Maps"
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d833115.1421612562!2d12.512323121739673!3d52.33392027009569!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47a84e373f035901%3A0x42120465b5e3b70!2sBerlin!5e0!3m2!1sen!2sde!4v1689325445609!5m2!1sen!2sde"
+                width="100%"
+                height="450"
+                style={{ border: 0 }}
+                allowFullScreen=""
+                loading="lazy"
+                referrerPolicy="no-referrer-when-downgrade"
+              ></iframe>
+            </div>
+          </div>
+        </div>
+      </div>
+    </Wrapper>
+  );
+};
+
+const Wrapper = styled.section`
+  background-image: url(${contactbg});
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-position: center;
+  min-height: 100vh;
+
+  .btn {
+    padding: 0.75rem 1.5rem;
+    font-size: 1rem;
+    background-color: var(--clr-button);
+    color: var(--clr-grey-1);
+    text-transform: capitalize;
+    letter-spacing: var(--spacing);
+    cursor: pointer;
+    transition: var(--transition);
+  }
+
+  .btn:hover {
+    background-color: #397f84;
+    color: var(--clr-white);
+  }
+
+  a {
+    color: #397f84;
+  }
+
+  a:hover {
+    color: var(--clr-red-light);
+  }
+
+  .social-icons {
+    display: flex;
+    gap: 1rem;
+    justify-content: center;
+    margin-top: 1rem;
+  }
+
+  .social-icons a {
+    transition: transform 0.3s;
+  }
+
+  .social-icons a:hover {
+    transform: scale(1.1);
+  }
+`;
 
 export default Contact;
