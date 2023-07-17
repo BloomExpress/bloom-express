00412af156aba4907a2418961fc551ab3bf5428d
Jamal <sayedjamal.ahmadi@dci-student>
2023-07-17 16:21:19 +0200

    Product List

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,10 +1,30 @@
 import { useEffect, useState } from "react";
 import { NavLink } from "react-router-dom";
 import { FaEllipsisV, FaShoppingCart } from "react-icons/fa";
+import flowers from "../../utils/flowers";
 
 const Products = () => {
-  const [plants, setPlants] = useState([]);
+  const [selectedCategory, setSelectedCategory] = useState("");
 
+  const categories = flowers.reduce((accu, flower) => {
+    if (!accu.includes(flower.category)) {
+      accu.push(flower.category);
+    }
+    return accu;
+  }, []);
+
+  const changeCategory = (e) => {
+    let category = e.target.value;
+    setSelectedCategory(category);
+  };
+
+  const filteredFlower = selectedCategory
+    ? flowers.filter((flower) => flower.category === selectedCategory)
+    : flowers;
+
+  const cardBody = {
+    padding: "1rem 0.1rem",
+  };
   const cardContainer = {
     paddingBottom: "6rem",
   };
@@ -12,6 +32,7 @@ const Products = () => {
   const card = {
     width: "100%",
     textAlign: "left",
+    borderColor: "none !important",
   };
 
   const links = {
@@ -19,40 +40,6 @@ const Products = () => {
     flexFlow: "row no-wrap",
     justifyContent: "space-between",
   };
-  const proxyUrl = "https://corsproxy.io/?";
-  const apiUrl =
-    "https://trefle.io/api/v1/plants?token=B8rfAvBOfhSdMC6SENJFVSgcEnThvnHm6lKV-euxf-A";
-
-  // const apiUrl =
-  //   "https://perenual.com/api/species-list?page=1&key=sk-taE064b1d4d3ae8921564";
-  /*
-    The Plant Encyclopedia API:
-    token=B8rfAvBOfhSdMC6SENJFVSgcEnThvnHm6lKV-euxf-A
-    This API provides information about various plants, including flowers.
-    It offers details such as scientific names, common names, descriptions, images, and more.
-      API Endpoint: https://trefle.io/api/plants
-      Documentation: https://trefle.io/
-
-    The OpenFarm API:
-
-    OpenFarm is a community-driven platform that offers information about farming and gardening.
-    It includes details about different types of plants, including flowers.
-
-      API Endpoint: https://openfarm.cc/api/v1/crops
-      Documentation: https://openfarm.cc/api/#introduction
-
-    3- perenual.com
-    sk-taE064b1d4d3ae8921564
-    */
-  useEffect(() => {
-    const getData = async () => {
-      const response = await fetch(proxyUrl + encodeURIComponent(apiUrl));
-      const data = await response.json();
-      console.log(data.data);
-      setPlants(data.data);
-    };
-    getData();
-  }, []);
 
   return (
     <>
@@ -65,13 +52,14 @@ const Products = () => {
                   className="form-select"
                   id="floatingSelect"
                   aria-label="Select Category"
+                  onChange={changeCategory}
                 >
-                  <option selected value="s">
-                    Spring
-                  </option>
-                  <option value="1">Summer</option>
-                  <option value="2">Winter</option>
-                  <option value="3">Falls</option>
+                  <option value="all">all</option>
+                  {categories.map((category, index) => (
+                    <option key={index} value={category}>
+                      {category}
+                    </option>
+                  ))}
                 </select>
                 <label htmlFor="floatingSelect" className="">
                   Select Category
@@ -81,46 +69,30 @@ const Products = () => {
           </div>
           <div className="col-md-10">
             <div className="row mt-3">
-              {plants.map((plant) => (
-                <div className="col-md-6" style={cardContainer} key={plant.id}>
-                  <div className="card" style={card}>
-                    <img
-                      src={plant.image_url}
-                      // src={plant.default_image.original_url}
-                      className="card-img-top"
-                      alt="Card Image"
-                      style={{
-                        width: "100%",
-                        height: "21rem",
-                        objectFit: "cover",
-                      }}
-                    />
-                    <div className="card-body p-4">
-                      <h5 className="card-title fs-3">{plant.common_name}</h5>
-                      <p className="card-text fs-4">{plant.genus}</p>
-                      <div style={links}>
-                        <NavLink to="/">
-                          <span
-                            style={{
-                              display: "flex",
-                              justifyContent: "center",
-                              alignItems: "center",
-                            }}
-                          >
-                            <FaEllipsisV />{" "}
-                            <span style={{ fontSize: "1.2rem" }}>
-                              See Details
-                            </span>
-                          </span>
-                        </NavLink>
-                        <NavLink to="/">
-                          <span>
-                            <FaShoppingCart /> Add to Cart
-                          </span>
-                        </NavLink>
+              {filteredFlower.map((f) => (
+                <div className=" col-md-4" style={cardContainer} key={f.id}>
+                  <NavLink to={`/product/${f.id}`}>
+                    <div style={card}>
+                      <img
+                        src={f.image}
+                        className="card-img-top"
+                        alt="Card Image"
+                        style={{
+                          width: "100%",
+                          height: "13rem",
+                          objectFit: "cover",
+                        }}
+                      />
+                      <div style={cardBody}>
+                        <div style={links}>
+                          <h5 className="card-title fs-5 text-black">
+                            {f.name}
+                          </h5>
+                          <p className="card-text fs-6">{f.price}$</p>
+                        </div>
                       </div>
                     </div>
-                  </div>
+                  </NavLink>
                 </div>
               ))}
             </div>
