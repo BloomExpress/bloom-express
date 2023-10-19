ba15e30c00adbe81c4930974b01125ba33273a23
manoloscipi <manoloscipi@gmail.com>
2023-10-19 13:52:02 +0200

    Contact model created

---
+++ backend/Models/Contact.js
@@ -0,0 +1,14 @@
+import { model, Schema } from "mongoose";
+
+const contactSchema = new Schema(
+  {
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
+    email: { type: String, required: true },
+    message: { type: String, required: true },
+  },
+  { timestamps: true }
+);
+
+const Contact = model("contact", contactSchema);
+export default Contact;
