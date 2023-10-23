7875c98660785276de9733a5d1b06a381e0d0a7e
ApiradeeH <apiradeehaeusler@gmail.com>
2023-10-23 11:24:02 +0200

    newsletter functions added

--- backend/Controllers/NewsletterController.js
+++ backend/Controllers/NewsletterController.js
@@ -18,3 +18,33 @@ export const createNewsletter = async (req, res) => {
       .json({ message: error.toString() });
   }
 };
+
+export const getAllNewsletters = async (req, res) => {
+  try {
+    const newsletters = await Newsletter.find();
+
+    return res.status(StatusCodes.OK).json(newsletters);
+  } catch (error) {
+    return res
+      .status(StatusCodes.INTERNAL_SERVER_ERROR)
+      .json({ message: error.toString() });
+  }
+};
+
+export const deleteNewsletterById = async (req, res) => {
+  try {
+    const newsletter = await Newsletter.findByIdAndDelete(req.params.id);
+
+    if (!newsletter) {
+      return res
+        .status(StatusCodes.NOT_FOUND)
+        .json({ message: "newsletter not found" });
+    }
+
+    return res.status(StatusCodes.OK).send({ message: "Newsletter deleted" });
+  } catch (error) {
+    return res
+      .status(StatusCodes.INTERNAL_SERVER_ERROR)
+      .json({ message: error.toString() });
+  }
+};
--- backend/Routes/newsletterRoute.js
+++ backend/Routes/newsletterRoute.js
@@ -1,8 +1,16 @@
 import express from "express";
-import { createNewsletter } from "../Controllers/NewsletterController.js";
+import {
+  createNewsletter,
+  getAllNewsletters,
+  deleteNewsletterById,
+} from "../Controllers/NewsletterController.js";
 
 const router = express.Router();
 
 router.post("/create", createNewsletter);
+
+router.get("/allNewsletters", getAllNewsletters);
+
+router.delete("/delete/:id", deleteNewsletterById);
 
 export default router;
