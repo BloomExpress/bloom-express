22c722756955543af9617cc0495ba29336eb94e5
ApiradeeH <apiradeehaeusler@gmail.com>
2023-10-26 21:24:06 +0200

    Faqs added

--- src/components/Faq.jsx
+++ src/components/Faq.jsx
@@ -1,7 +1,44 @@
-import Modal from 'react-modal';
-import styled from 'styled-components';
+import { useEffect, useState } from "react";
+import Modal from "react-modal";
+import styled from "styled-components";
+import axios from "../utils/axiosInstance";
 
 const FaqModal = ({ isOpen, onRequestClose }) => {
+  const limit = 5;
+  const [data, setData] = useState([]);
+  const [skip, setSkip] = useState(0);
+
+  const handleNext = () => {
+    setSkip(skip + limit);
+    getAllFaqs();
+  };
+
+  const handlePrevious = () => {
+    if (skip - limit >= 0) {
+      setSkip(skip - limit);
+      getAllFaqs();
+    }
+  };
+
+  useEffect(() => {
+    const getAllFaqs = async () => {
+      try {
+        const response = await axios.get(
+          `/api/faqs/getAllFaqs?limit=${limit}&skip=${skip}`
+        );
+        console.log("API Response:", response.data); // Log the response data
+        if (response.data && response.data) {
+          setData(response.data);
+        } else {
+          console.error("No data found in the API response.");
+        }
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+    getAllFaqs();
+  }, [skip]);
+
   return (
     <Wrapper>
       <Modal
@@ -11,10 +48,23 @@ const FaqModal = ({ isOpen, onRequestClose }) => {
       >
         <h2>Frequent questions and answers</h2>
         <Content>
-          <p></p>
+          <ul>
+            {data.map((faq) => (
+              <li key={faq._id}>
+                <p>
+                  <strong>Question:</strong> {faq.question}
+                </p>
+                <p>
+                  <strong>Answer:</strong> {faq.answer}
+                </p>
+              </li>
+            ))}
+          </ul>
         </Content>
 
         <Button onClick={onRequestClose}>Close</Button>
+        <Button onClick={handlePrevious}>Previous</Button>
+        <Button onClick={handleNext}>Next</Button>
       </Modal>
     </Wrapper>
   );
