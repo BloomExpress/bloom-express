d2ea15258a28c90354a9393c3d0f005a2d44738b
ApiradeeH <apiradeehaeusler@gmail.com>
2023-11-30 00:41:48 +0100

    try node fetch

--- client/src/components/NewsletterModal.jsx
+++ client/src/components/NewsletterModal.jsx
@@ -33,7 +33,7 @@ const Newsletter = () => {
         headers: {
           "Content-Type": "application/json",
         },
-        body: JSON.stringify(data),
+        body: JSON.stringify({ email: email }),
       });
 
       const data = await response.json();
