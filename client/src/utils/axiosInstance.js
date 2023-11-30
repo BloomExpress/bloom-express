9eabf9051a857fa3430371c3eee1c53f5da0b0f4
ApiradeeH <apiradeehaeusler@gmail.com>
2023-11-30 11:02:48 +0100

    credential set to false

--- client/src/utils/axiosInstance.js
+++ client/src/utils/axiosInstance.js
@@ -4,6 +4,6 @@ const axiosApiInstance = axios.create();
 
 //define our settings for our axios instance
 axiosApiInstance.defaults.baseURL = import.meta.env.VITE_BASE_URL; //location of our backend server
-axiosApiInstance.defaults.withCredentials = true;
+axiosApiInstance.defaults.withCredentials = false;
 
 export default axiosApiInstance;
