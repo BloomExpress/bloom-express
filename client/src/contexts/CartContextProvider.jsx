ef32de8b616452c25abca9b2e6757bba887573e2
ApiradeeH <apiradeehaeusler@gmail.com>
2023-11-30 10:20:28 +0100

    cart context changed to slocal storage

--- client/src/contexts/CartContextProvider.jsx
+++ client/src/contexts/CartContextProvider.jsx
@@ -7,14 +7,14 @@ const initialValue = {
   cart: [],
 };
 
-const getCartFromSessionStorage = () => {
-  const cart = sessionStorage.getItem("cart");
+const getCartFromLocalStorage = () => {
+  const cart = localStorage.getItem("cart");
 
   return cart ? JSON.parse(cart) : initialValue;
 };
 
 function CartContextProvider({ children }) {
-  const [state, dispatch1] = useReducer(cartReducer, getCartFromSessionStorage());
+  const [state, dispatch1] = useReducer(cartReducer, getCartFromLocalStorage());
 
   useEffect(()=>{
     localStorage.setItem("cart", JSON.stringify(state));
